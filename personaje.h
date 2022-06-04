78832a757461bc1bcb1e530570da34e1a11b449e
NolbertoCastro <nolber0703@gmail.com>
2022-06-04 14:43:44 -0500

    Le pusimos vida, daño y proteccion al personaje

--- personaje.h
+++ personaje.h
@@ -11,14 +11,21 @@ class Personaje{
         std::string nombre;
         Habitacion habitacionActual;
         Item inventario;
-
+        int vida;
+        int proteccion;
+        int daño;
     public:
         Personaje();
         Personaje(std::string, Habitacion, Item);
-
         std::string getNombre() const;
         Habitacion getHabitacionActual() const;
         Item getInventario() const;
+        int getvida();
+        int getproteccion();
+        int getdaño();
+        void setvida(int);
+        void setdaño(int);
+        void setproteccion(int);
         void setNombre(std::string);
         void setHabitacionActual(Habitacion);
         void setItem(Item);
