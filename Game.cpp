db3b8f16a1f8901a7f4839d44fdd71b61578f928
NolbertoCastro <nolber0703@gmail.com>
2022-06-04 14:23:06 -0500

    Modificando el codigo

--- Game.cpp
+++ Game.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class Game {
+    public:
+        Game();
+        void creaElementos();
+        void creaComandos();
+        void play();
+        //bool procesaComando(Comando*);
+        void imprimeBienvenida();
+    private:
+        //Parser parser;
+        //Character* personaje;
+        //Room *sala, *comedor, *jardin;
+        //Item *pocion, *libro, *monedas, *llave; 
+};
 
-int inicio(){
+void Game::imprimeBienvenida(){
     cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" << endl;
     cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" << endl;
     cout << "░░░░░░░░░░█░░░░░░█░░░█░░░█░░░░░██░░░██████░░███░░░████████░░██████░░░░░█░░░░░░" << endl;
@@ -30,10 +44,7 @@ int inicio(){
     cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░███░░░░░░" << endl;
     cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" << endl;
     cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" << endl;
-};
 
-int main(){
-    inicio();
     cout << "\nBienvenido a ASUSTATEC el videojuego de Terror en el Tecnologico de Monterrey" << endl;
     cout << "\nEn este videojuego tendrás que pelear contra enemigos, ganar dinero y comprar items para mejorar tu personaje." << endl;
     cout << "\nPara ganar tendrás que ganar/comprar una tarjeta de alumno tec para acceder al elevador y ganar, a continuación te mostramos el mapa."  << endl;
@@ -63,7 +74,7 @@ int main(){
     cout << "\nEmpezarás en al inicio del pasillo localizado al lado del cuarto #5, tendrás que recorrer los cuartos, vencer enemigos, comprar objetos." << endl;
     cout << "\nTodo con el objetivo de conseguir la tarjeta de alumno tec para acceder al elevador." << endl;
     cout << "\nPara poder desplazarte por el mapa podras usar los siguientes comandos:" << endl;
-    
+
     cout << "\nMover N = Arriba" << endl;
     cout << "Mover S = Abajo" << endl;
     cout << "Mover O = Izquierda" << endl;
